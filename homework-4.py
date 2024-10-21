8d01708eba91643c22ab6c1cbb79277fbc0e8e8a
happy2111 <muhammadyusufa2111@gmail.com>
2024-10-21 21:11:25 +0500

    Solving tasks with Python using integers.

---
+++ homework-4.py
@@ -0,0 +1,162 @@
+# integer 1
+# L = int(input("Enter metr: "))
+# match = L * 100
+# print(f"{L}m = {match}cm")
+
+
+# integer 2
+# M = int(input("Enter tonna: "))
+# kg = M * 1000
+# print(f"{M}t = {kg}kg")
+
+
+# integer 3
+# kb = int(input("Enter kb: "))
+# bayt = kb * 1024
+# print(f"{kb}kb = {bayt}bayt")
+
+
+# integer 4 
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# match = a / b
+# print(match)
+
+
+# integer 5
+# a = int(input('Enter a: '))
+# b = int(input('Enter b: '))
+# check = a // b
+# print(check)
+
+# integer 6
+# num = int(input("Enter number: "))
+# onlik = num % 100 // 10
+# birlik = num % 10
+# print(f"{onlik}\n{birlik}")
+
+
+# integer 7 
+# num = int(input("Enter a two-digit number: "))
+# tens = num % 100 // 10
+# ones = num % 10
+# summ = tens + ones
+# print(f"Your Entered number: {num}\ntens = {tens}\nones = {ones}\n{ones} + {tens} = {summ}")
+
+
+# integer 8 
+# num = int(input("Enter a two-digit number: "))
+# tens = num % 100 // 10
+# ones = num % 10
+# print(f"{ones}{tens}")
+
+
+# integer 9
+# num = int(input("Enter a tree-digit number: "))
+# hundreds = num // 100
+# print(hundreds)
+
+
+# integer 10 
+# num = int(input("Enter tree-digit number: "))
+# ones = num % 10
+# tens = (num % 100) // 10
+# print(f"ones = {ones} tens = {tens}")
+
+
+# integer 11
+# num = int(input("Enter a tree-digit number: "))
+# ones = num % 10
+# tens = (num % 100) // 10
+# hundereds = num // 100
+# summ = ones + tens + hundereds 
+# print(f"ones = {ones} tens = {tens} hundereds = {hundereds} summ = {summ}")
+
+
+# integer 12 
+# num = int(input("Enter a tree-digit number: "))
+# ones = num % 10
+# tens = (num % 100) // 10
+# hundereds = num // 100
+# printing = f"{ones}{tens}{hundereds}"
+# print(printing)
+
+
+# integer 13 
+# num = int(input("Enter a tree-digit number: "))
+# hundereds = num // 100
+# tens = num % 100 // 10
+# ones = num % 10
+# printing = f"{tens}{ones}{hundereds}"
+# print(printing)
+
+
+# integer 14
+# num = int(input("Enter a tree-digit number: "))
+# hundereds = num // 100
+# tens = num % 100 // 10
+# ones = num % 10
+# printing = f"{ones}{hundereds}{tens}"
+# print(printing)
+
+
+# integer 15 
+# num = int(input("Enter a tree-digit number: "))
+# hundereds = num // 100
+# tens = num % 100 // 10
+# ones = num % 10
+# printing = f"{ones}{tens}{hundereds}"
+# print(printing)
+
+
+# integer 16 
+# num = int(input("Enter a tree-digit number: "))
+# hundereds = num // 100
+# tens = num % 100 // 10
+# ones = num % 10
+# printing = f"{hundereds}{ones}{tens}"
+# print(printing)
+
+
+# integer 17
+# num = int(input("Son kiriting: "))
+# hundreds = (num // 100) % 10
+# print(hundreds)
+
+
+# integer 18
+# num = int(input("Son kiriting: "))
+# first = num // 1000
+# print(first)
+
+
+# integer 19
+# sec = int(input("Enter seconds: "))
+# conventor = sec / 60 
+# print(conventor)
+
+
+# integer 20 
+# sec = int(input("Enter a seconds: "))  
+# convertor = (sec / 60) / 60 
+# print(convertor)
+
+
+# integer 21 
+# sec = int(input("Enter seconds: "))
+# conventor = sec / 60 
+# print(f"min: {conventor} sec: {sec}")
+
+
+# integer 22
+# sec = int(input("Enter a seconds: "))  
+# convertor = (sec / 60) / 60 
+# print(f"hour: {conventor} sec: {sec}")
+
+
+# integer 23
+# sec = int(input("Enter seconds: "))
+# min = sec / 60 
+# hour = (sec / 60) / 60 
+# print(f"hour: {hour} min: {min} sec: {sec}")
+
